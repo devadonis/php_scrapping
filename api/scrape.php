82a8cd50365064425dedba7178f3e0651f96f930
devadonis <devadonis00@gmail.com>
2023-09-08 05:54:53 +0900

    Add checkRequest function to check incoming url

--- api/scrape.php
+++ api/scrape.php
@@ -18,16 +18,19 @@
   }
 
   function checkRequest($domain, $url, $element) {
-    file_put_contents("error.log", print_r($element,true)."\n", FILE_APPEND);
-    
-    $sql = "SELECT domain.name, url.name, element.name, requests.time, requests.duration FROM (((requests LEFT JOIN domain ON requests.domain_id = domain.id ) LEFT JOIN url ON requests.url_id = url.id) LEFT JOIN element ON requests.element_id = element.id) ORDER BY requests.time DESC LIMIT 1";
-    // file_put_contents("error.log", print_r($connection,true)."\n", FILE_APPEND);
+    $query = "SELECT requests.id, domain.name as domain, url.name AS url, element.name AS element, requests.time, requests.duration
+      FROM requests
+      LEFT JOIN domain ON requests.domain_id = domain.id
+      LEFT JOIN url ON requests.url_id = url.id
+      LEFT JOIN element ON requests.element_id = element.id
+      WHERE domain.name=? AND url.name=? AND element.name=?
+      ORDER BY requests.time DESC LIMIT 1";
 
-    $result = Database::$connection->query($sql);
+    $result = Database::$connection->execute_query($query, [$domain, $url, $element])->fetch_assoc();
     
-    if ($result->num_rows > 0) {
-      file_put_contents("error.log", print_r($result,true)."\n", FILE_APPEND);
+    if ($result) {
       // return old value and upgrade time
+        file_put_contents("error.log", print_r($result,true)."\n", FILE_APPEND);
     } else {
       // scrape data
     }
