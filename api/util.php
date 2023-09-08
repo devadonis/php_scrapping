99c03932203d8d907bf8d8d412440c6a05c81fd4
acedev0816 <acedev0816@gmail.com>
2023-09-08 16:25:09 +0900

    add function

--- api/util.php
+++ api/util.php
@@ -6,3 +6,10 @@
 
     return array("domain"=>$domain, "path"=>$path);
   }
+
+  function queryResultToArray($result){
+    $rows = [];
+    while ($row = $result->fetch_assoc())
+      $rows[] = $row;
+    return $rows;
+  }
