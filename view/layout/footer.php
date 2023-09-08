a13ce2152bcba3a006d20041101ba55fd17f7b52
acedev0816 <acedev0816@gmail.com>
2023-09-08 11:57:19 +0900

    work

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -1,4 +1,3 @@
-  <script src="<?php echo ASSETS; ?>/js/jquery-3.7.1.min.js"></script>
   <script src="<?php echo ASSETS; ?>/js/custom.js"></script>
 </body>
 </html>
--- view/statistic.php
+++ view/statistic.php
@@ -6,27 +6,28 @@
     </div>
     <form class="big-form">
       <label for="domain"> Select Domain(120) </label>
-      <select name="domain">
+      <select id="domain" name="domain">
         <option>Google.com</option>
       </select>
       <p>Average fetch time from domain</p>
       <p class="result">24s</p>
       <p>Urls fetched from domain</p>
-      <p class="result">2500</p>
+      <p class="result" id="urls_from_domain">___</p>
       <hr class="separator" />
       <label for="domain"> Select Element(30) </label>
-      <select name="element">
+      <select name="element" id="element">
         <option>div</option>
         <option>label</option>
       </select>
       <p>Total &lt;img&gt; elements fetched from google.com</p>
-      <p class="result">245</p>
+      <p class="result" id="elements_from_domain">___</p>
       <p>Total &lt;img&gt; elements fetched so far</p>
-      <p class="result">2435</p>
+      <p class="result" id="elements_so_far">___</p>
       <input type="submit" value="Submit">
     </form>
   </div>
 </div>
 </main>
 <!-- script goes here -->
+<script src="<?php echo ASSETS; ?>/js/statistic.js"></script>
 <?php require "layout/footer.php" ?>
